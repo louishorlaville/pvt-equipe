40c451eefd6c28833d2be87c93a75f435261aae7
Louis Horlaville <louis.horlaville@gmail.com>
2021-01-28 16:04:46 -0500

    changé message de bienvenue

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const nom = "Monde";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Allo {nom}
         </p>
         <a
           className="App-link"
